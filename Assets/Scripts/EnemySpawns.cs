e451a8eb93579926bd428b15d50c2b2268c7ffd5
Cornelius-github <85209828+Cornelius-github@users.noreply.github.com>
2021-11-30 10:02:43 +1100

    tutorial basically done

--- Assets/Scripts/EnemySpawns.cs
+++ Assets/Scripts/EnemySpawns.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemySpawns : MonoBehaviour
 {
@@ -11,14 +12,18 @@ public class EnemySpawns : MonoBehaviour
 
     bool isCreated = false;
 
+    private NavMeshAgent navEnemy;
+
     // Update is called once per frame
     void Update()
     {
+        navEnemy = GetComponent<NavMeshAgent>();
+        navEnemy.isStopped = true;
         if (isCreated == false)
         {
+            navEnemy.isStopped = false;
             if (player.position.x > entersMaze.position.x || player.position.z > entersMaze.position.z)
             {
-                //Instantiate(enemy, enemySpawnpoint.position, Quaternion.identity);
                 enemy.position = enemySpawnpoint.position;
                 isCreated = true;
             }
